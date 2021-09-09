0cd8974ef001f292268168ed5cf5e86d739f2856
evhen <evhen@mail.com>
2021-09-09 13:12:56 +0300

    minor changes according to PSR4

--- src/HW4/Color.php
+++ src/HW4/Color.php
@@ -8,7 +8,8 @@ class Color
     private int $green;
     private int $blue;
 
-    function __construct(int $red, int $green, int $blue) {
+    public function __construct(int $red, int $green, int $blue)
+    {
         $this->setRed($red);
         $this->setGreen($green);
         $this->setBlue($blue);
@@ -19,7 +20,6 @@ class Color
         return $this->getRed() == $color->getRed()
             && $this->getGreen() == $color->getGreen()
             && $this->getBlue() == $color->getBlue();
-
     }
 
     public function mix(Color $newColor): Color
@@ -27,7 +27,8 @@ class Color
         return new Color(
             intdiv($newColor->getRed() + $this->getRed(), 2),
             intdiv($newColor->getGreen() + $this->getGreen(), 2),
-            intdiv($newColor->getBlue()+$this->getBlue(), 2));
+            intdiv($newColor->getBlue() + $this->getBlue(), 2)
+        );
     }
 
     public static function randomColor(): self
@@ -35,44 +36,43 @@ class Color
         return new self(rand(0, 255), rand(0, 255), rand(0, 255));
     }
 
-    public function getRed (): int
+    public function getRed(): int
     {
         return $this->red;
     }
 
-    public function getGreen (): int
+    public function getGreen(): int
     {
         return $this->green;
     }
 
-    public function getBlue (): int
+    public function getBlue(): int
     {
         return $this->blue;
     }
 
-    private function setRed (int $red): void
+    private function setRed(int $red): void
     {
         $this->checkColorExist($red);
         $this->red = $red;
     }
 
-    private function setGreen (int $green): void
+    private function setGreen(int $green): void
     {
         $this->checkColorExist($green);
         $this->green = $green;
     }
 
-    private function setBlue (int $blue): void
+    private function setBlue(int $blue): void
     {
         $this->checkColorExist($blue);
         $this->blue = $blue;
     }
 
-    private function checkColorExist ($color): void
+    private function checkColorExist($color): void
     {
         if ($color < 0 || $color > 255) {
             exit('Такого цвета не существует');
         }
     }
-
 }
